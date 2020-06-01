b5c5cd70ff6224517d8f1e339704d4c341c88160
RafaelECGM23004 <rafaelaquino@ipvc.pt>
2020-06-01 15:34:06 +0100

    Primeiro Upload de códigos, já com o nome do OuriRegras traduzido.

---
+++ src/tp_ouri/TestandVerifyMancalaBuild.java
@@ -0,0 +1,55 @@
+package tp_ouri;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestandVerifyMancalaBuild {
+	
+	@Test
+	  public void callMethodMancalaRule() {
+	
+		  OuriRegras.printGameRules();
+	  }
+	  @Test
+	  public void callPlayMancalaWithComputer() throws IOException {
+		
+		  PlayAndBuildMancalaGame playGameMancals = new PlayAndBuildMancalaGame(null, null,"Y");
+		  playGameMancals.playMancala();
+	  }
+	  /*@Test
+	  public void callPlayMancalaWithComputerComputer() throws IOException {
+		
+		  PlayAndBuildMancalaGame playGameMancals = new PlayAndBuildMancalaGame("Shashi", null,"Y");
+		  playGameMancals.playMancala();
+	  }
+	  @Test
+	  public void callPlayMancalaWithHuman() throws IOException {
+		
+		  PlayAndBuildMancalaGame playGameMancals = new PlayAndBuildMancalaGame("Shashi", "Aryan","Y");
+		  playGameMancals.playMancala();
+	  }
+	  
+	  @Test
+	  public void callPlayMancalaWithHumanWithSuffle() throws IOException {
+		
+		  PlayAndBuildMancalaGame playGameMancals = new PlayAndBuildMancalaGame("Shashi", "Aryan","Y");
+		  playGameMancals.playMancala();
+	  }
+	  @Test
+	  public void callPlayMancalaWithHumanWithoutSuffle() throws IOException {
+		
+		  PlayAndBuildMancalaGame playGameMancals = new PlayAndBuildMancalaGame("Shashi", "Aryan","N");
+		  playGameMancals.playMancala();
+	  }*/
+	  @BeforeTest
+	  public void beforeTest() {
+	  }
+
+	  @AfterTest
+	  public void afterTest() {
+	  }
+
+}
